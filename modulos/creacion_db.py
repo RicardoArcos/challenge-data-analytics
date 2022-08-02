0115459a6638d6e04b4fc104feef1bd4a85df1ff
Ricardo Arcos <ingricardoarcos@gmail.com>
2022-08-02 00:09:33 -0500

    modifying the db creation file

--- modulos/creacion_db.py
+++ modulos/creacion_db.py
@@ -1,9 +1,6 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer
 from sqlalchemy import create_engine
 
 from decouple import config
 
-Base = declarative_base()
 engine = create_engine(f"postgresql://{config('USER')}:{config('PASSWORD')}@{config('SERVER')}:{config('PORT')}/{config('DB_NAME')}")
 
