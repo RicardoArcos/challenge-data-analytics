8ac0b9a25bd96f7d367f2891edcf88aaa564d7f8
Ricardo Arcos <ingricardoarcos@gmail.com>
2022-08-17 22:01:05 -0500

    adding docstrings

--- creacion_bd.py
+++ creacion_bd.py
@@ -7,6 +7,8 @@ from decouple import config
 from config import *
 
 def get_engine():
+    ''' Se crea la conexión a la base de datos a partir del usuario, contraseña, host, puerto y nombre
+    proporcinados por el usuario. Retorna un elemento de conexión.'''
     url = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
     if not database_exists(url):
         create_database(url)
@@ -16,12 +18,14 @@ def get_engine():
 engine = get_engine()
 
 def create_tables():
+    ''' Esta función crea las tablas de la base de datos en base a los scripts sql. '''
     with engine.connect() as con:
         with open("scripts_sql/tablas.sql") as file:
             consulta = text(file.read())
             con.execute(consulta)
             
 def add_columns():
+    ''' Se añaden las columnas extras a las tablas de la base de datos. '''
     with engine.connect() as con:
         with open("scripts_sql/nueva_columna.sql") as file:
             consulta = text(file.read())
